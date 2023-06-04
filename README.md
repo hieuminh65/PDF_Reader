45307a3dc512bcf6d7805edc2acc226020f366ff
hieuminh65 <hieuminhnguyen@usf.edu>
2023-06-03 21:31:54 -0400

    Final Update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PDF_Reader
-A PDF_Reader web app built with Streamlit and Python
+A PDF_Reader web app built with Streamlit, Python, and Langchain. Feel free it use this as your own.
+
+## How it works?
+When you upload a document, it gets split into smaller sections and stored in a special kind of database called a vector index. This type of database allows for semantic search and retrieval, which means it can find related information even if the individual words aren't exact matches. 
+
+When you ask a question, the model searches through these document sections using the vector index to find relevant information and ultimately provide an answer.
+
+![Web Demo](img.png)
+
+## Usage 
+1. ```git clone https://github.com/hieuminh65/PDF_Reader.git```
+2. ```pip3 install -r requirements.txt```
+3. Paste your API keys in the .env.example file and rename the file to .env
+4. Run ```streamlit run streamlit__app.py```
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,24 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-import openai
+
+with st.sidebar:
+    st.markdown('''
+    ## How this works
+    When you upload a document, it gets split into smaller sections and stored in a special kind of database called a vector index. This type of database allows for semantic search and retrieval, which means it can find related information even if the individual words aren't exact matches. 
+
+    When you ask a question, the model searches through these document sections using the vector index to find relevant information and ultimately provide an answer.
+
+    ## About me
+    Check out my [Web](https://mywebleo.com)
+    ''')
+    
+
 
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title = "PDF Reader")
     st.title("PDF Reader")
-    st.header("Created by Leo.")
-
+    load_dotenv()
     pdf = st.file_uploader("Upload your PDF file", type = ["pdf"])
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -23,6 +32,7 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()  
         
+        #split text into chunks
         text_splitter = CharacterTextSplitter(
             separator= "\n",
             chunk_size = 1000,
@@ -36,7 +46,7 @@ def main():
         document = FAISS.from_texts(chunks, embeddings)
         
         #search
-        user_question = st.text_input("Enter your question about the pdf")
+        user_question = st.text_input("Enter your question about the pdf: ")
         if user_question:
             docs = document.similarity_search(user_question, k = 3)
             llm = OpenAI()
